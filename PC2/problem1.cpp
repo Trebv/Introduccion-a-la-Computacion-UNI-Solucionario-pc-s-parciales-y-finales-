6d149758709ea88a89d36700ed6530e1e3ebc87a
treb <bjvc1104@gmail.com>
2026-01-06 16:30:09 -0500

    calculo de promedio

--- PC2/problem1.cpp
+++ PC2/problem1.cpp
@@ -6,15 +6,15 @@ int main(){
     int numsOfUser[100];
     int index = 0;
     do {
-	std::cout << "Digite un numero (0-100):\n";
+	std::cout << "Digite una nota (0-100):\n";
 	std::cin >> numUser;
 	if (numUser < 0 ){
 	    std::cout << "Saliendo del programa...\n";
 	    break;
 	}
 	if ( numUser < 0 || numUser > 100 ) {
-	    std::cout << "El numero ingresado no esta entre 0 a 100.\n"
-			 "Por favor vuelva a ingresar un nuevo numero.\n";
+	    std::cout << "La nota ingresada no esta entre 0 a 100.\n"
+			 "Por favor vuelva a ingresar una nueva nota.\n";
 	    continue;
 	}
 	numsOfUser[index] = numUser;
@@ -26,7 +26,14 @@ int main(){
     for (int i = 0; i < counter; i++){
 	std::cout << numsOfUser[i] << "\n";
     }
-    std::cout << "cantidad de numeros ingresados: " << counter << "\n";
+    std::cout << "cantidad de notas ingresados: " << counter << "\n";
+    
+    float average = 0;
+    for (int i = 0; i < counter; i++){
+	average += numsOfUser[i];
+    }
+    average /= counter;
+    std::cout << "Promedio: " << average << "\n";
 
 
     return 0;
