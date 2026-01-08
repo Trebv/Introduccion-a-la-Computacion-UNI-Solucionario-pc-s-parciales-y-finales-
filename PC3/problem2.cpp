476580297bdbe8c17c22bc3eff98a9591b00d4a7
treb <106878698+Trebv@users.noreply.github.com>
2026-01-08 10:00:13 -0500

    correcion de indentacion

--- PC3/problem2.cpp
+++ PC3/problem2.cpp
@@ -5,39 +5,39 @@ int main (){
 
     std::cout << "Introduce los 6 elementos de la matriz 3x2 (fila por fila):\n";
     for ( int i = 0; i < 3; i++ ) {
-	for ( int j = 0; j < 2; j++ ) {
-	    std::cin >> num;
-	    array[i][j] = num;
-	}
+		for ( int j = 0; j < 2; j++ ) {
+	    	std::cin >> num;
+	    	array[i][j] = num;
+		}
     }
     
     std::cout << "Matriz Original: \n";
     for ( int i = 0; i < 3; i++ ) {
-	for (int j = 0; j < 2; j++ ) {
-	    std::cout << array[i][j] << "  ";
-	}
-	std::cout << "\n";
+		for (int j = 0; j < 2; j++ ) {
+	    	std::cout << array[i][j] << "  ";
+		}
+		std::cout << "\n";
     }
 
     std::cout << "Matriz Transpuesta: \n";
     
     for ( int j = 0; j < 2; j++ ) {
-	for (int i = 0; i < 3; i++ ) {
-	    std::cout << array[i][j] << "  ";
-	}
-	std::cout << "\n";
+		for (int i = 0; i < 3; i++ ) {
+	    	std::cout << array[i][j] << "  ";
+		}
+		std::cout << "\n";
     }
 
     int maxElement = array[0][0];
     int minElement = array[0][0];
     for ( int i = 0; i < 3; i++ ) {
-	for ( int j = 0; j < 2; j++ ) {
-	    if ( minElement > array[i][j] ) { minElement = array[i][j]; }
-	    if ( maxElement < array[i][j] ) { maxElement = array[i][j]; }
-	}
+		for ( int j = 0; j < 2; j++ ) {
+	    	if ( minElement > array[i][j] ) { minElement = array[i][j]; }
+	    	if ( maxElement < array[i][j] ) { maxElement = array[i][j]; }
+		}
     }
 
     std::cout << "Elemento Mayor en la Matriz Original: " << maxElement << "\n" <<
-		"Elemento Menor en la Matriz Original: " << minElement << "\n";
+				"Elemento Menor en la Matriz Original: " << minElement << "\n";
     return 0;
 }
