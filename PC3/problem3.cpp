eb6835690db59759d9795eb85e1db95d9c8cce76
treb <106878698+Trebv@users.noreply.github.com>
2026-01-08 10:02:47 -0500

    Fix formatting and indentation in problem3.cpp

--- PC3/problem3.cpp
+++ PC3/problem3.cpp
@@ -8,15 +8,14 @@ int main () {
     srand(time(NULL));
     
     for ( int i = 0; i < 5; i++) {
-	for ( int j = 0; j < 5; j++ ) {
-
+		for ( int j = 0; j < 5; j++ ) {
 	    array[i][j] = (rand() % 100) + 1;
-	}
+		}
     }
     numMayorDiagonal = array[0][0];
 
     for (int i = 0; i < 5; i++) {
-	if ( numMayorDiagonal < array[i][i] ) { numMayorDiagonal = array[i][i]; }
+		if ( numMayorDiagonal < array[i][i] ) { numMayorDiagonal = array[i][i]; }
     }
     std::cout << "Numero mas grande en la diagonal principal: " << numMayorDiagonal << "\n";
     
