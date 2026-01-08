4ac6d2cd0f3beec672a6b7bb35d8b3497e6d5325
treb <106878698+Trebv@users.noreply.github.com>
2026-01-08 10:04:20 -0500

    indentacion

--- PC3/problem4.cpp
+++ PC3/problem4.cpp
@@ -9,17 +9,17 @@ int main(){
     int distanciaCiudadMasCercana = 0;
     int minDiferencia = 200;
     for (int i = 0; i < 10; i++){
-	if( distanciaCli == distancias[i] ) {
-	    std::cout << "Se encontro una coincidencia con una ciudad a una distancia de " << distanciaCli << "km.\n";
-	    return 0;
-	}
-	if ( minDiferencia > std::abs(distancias[i] - distanciaCli) ) { 
-	    minDiferencia = std::abs(distancias[i] - distanciaCli); 
-	    distanciaCiudadMasCercana = distancias[i];
-	}
+		if( distanciaCli == distancias[i] ) {
+	    	std::cout << "Se encontro una coincidencia con una ciudad a una distancia de " << distanciaCli << "km.\n";
+	    	return 0;
+		}
+		if ( minDiferencia > std::abs(distancias[i] - distanciaCli) ) { 
+	    	minDiferencia = std::abs(distancias[i] - distanciaCli); 
+	    	distanciaCiudadMasCercana = distancias[i];
+		}
     }
 
     std::cout << "No se encontro una coincidencia exacta.\n" <<
-		"La ciudad mas cercana esta a " << distanciaCiudadMasCercana << "km.\n"; 
+				"La ciudad mas cercana esta a " << distanciaCiudadMasCercana << "km.\n"; 
     return 0;
 }
