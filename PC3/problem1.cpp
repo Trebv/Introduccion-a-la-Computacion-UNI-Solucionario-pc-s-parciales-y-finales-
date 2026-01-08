f8c799e41902a791e8053cca8f63ac81dbabc575
treb <106878698+Trebv@users.noreply.github.com>
2026-01-08 09:58:58 -0500

    corrigiendo indentacion
    Parece que hacer desde nvim la indentacion no me la cosidera xd, recien me doy cuente. Lo ire revisando en cada sol

--- PC3/problem1.cpp
+++ PC3/problem1.cpp
@@ -4,17 +4,18 @@ int main() {
     int num_M = 0, num_m = 0;
     // Almacenar cada numero en el array
     for (int i = 0; i < 5; i++) {
-	std::cout << "Ingrese un numero: ";
-	int num = 0;
-	std::cin >> num;
-	if ( i == 1) {
-	    num_M = num;
-	    num_m = num;
-	}
-	array[i] = num;
+		std::cout << "Ingrese un numero: ";
+		int num = 0;
+		std::cin >> num;
+		
+		if ( i == 1) {
+	    	num_M = num;
+	    	num_m = num;
+		}
+		array[i] = num;
 
-	if ( num_M < num ) { num_M = num; }
-	if ( num_m > num ) { num_m = num; }
+		if ( num_M < num ) { num_M = num; }
+		if ( num_m > num ) { num_m = num; }
     }
     
     std::cout << "Numero mas grande del arreglo: " << num_M << "\n"<<
