e63f4630fbf39bd97ec20d130c7b44bb8c1a539d
treb <106878698+Trebv@users.noreply.github.com>
2026-01-11 11:12:53 -0500

    Refactor input handling and binary conversion logic

--- 2025_1/PC2/problem4.cpp
+++ 2025_1/PC2/problem4.cpp
@@ -1,3 +1,4 @@
+// En proceso.
 #include <iostream>
 #include <string>
 
@@ -6,21 +7,22 @@ int main(){
     int userNum = 0;
     std::string binNum = "";
     while( true ) {
-	std::cout << "Ingrese un numero entero positivo: ";
-	std::cin >> userNum;
-	if ( userNum <= 0 ) { continue; }
+		std::cout << "Ingrese un numero entero positivo: ";
+		std::cin >> userNum;
+		if ( userNum <= 0 ) { continue; }
 	
-	while ( true ) {
-	    if (userNum / 2 < 2){
-		break;
+		while ( true ) {
+	    	if (userNum / 2 < 2){
+			break;
 	    }
 	    binNum += ( userNum % 2);
 	}
+	
 	std::cout << "Representacion binaria de " << userNum << ": " << binNum << "\n";
 	std::cout << "Desea convertir otro numero? (y/n): ";
 	std::getline(std::cin >> std::ws, userInput);
 
-	if (userInput 
+		if (userInput 
     }
     return 0;
 }
