a2ceac813d722cd5d82dfc2b3e9dc6c732792cba
treb <106878698+Trebv@users.noreply.github.com>
2026-01-11 10:48:27 -0500

    Refactor input prompts and comments for clarity

--- 2025_1/PC2/problem1.cpp
+++ 2025_1/PC2/problem1.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 
 int main(){    
-    
+
+	//Esta variable nos permite saber cuantos numeros validos estamos recibiendo.
     int counter = 0;
     float sumaDeNotas = 0;
 	
     do {
-		std::cout << "Digite una nota (0-100):\n";
+		std::cout << "Digite una nota (0-100).:\n";
 		float numUser = 0;
 		std::cin >> numUser;
 
-		// Si el input ingresado es un numero < 0, el programa finaliza.
+		// Si el input ingresado es un numero < 0, el bucle finaliza.
 		if (numUser < 0 ){ 
-	   		std::cout << "Saliendo del programa...\n";
-	    	return 0;
+	    	break;
 		}
 		if ( numUser > 100 ) {
 	    	std::cout << "Error: La nota ingresada no esta entre 0 a 100.\n"
@@ -24,7 +24,7 @@ int main(){
 		// con cada iteracion se pide un numero. Si el numero es valido [0-100], entonces acumulamos cada numero en la variable sumaDeNotas.
 		sumaDeNotas += numUser;
 
-		// El contador aumenta en 1. Esta variable nos permite saber cuantos numeros validos estamos recibiendo.
+		// El contador aumenta en 1. 
 		counter++;
     } while (true);
     
@@ -35,5 +35,6 @@ int main(){
 		float average = sumaDeNotas/(float)counter;
 		std::cout << "Promedio de notas: " << average << "\n";
     }
+	std::cout << "Saliendo del programa...\n";
     return 0;
 }
