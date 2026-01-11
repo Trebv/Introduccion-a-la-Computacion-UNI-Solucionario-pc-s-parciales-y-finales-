1c921f87ef60973d4204d0b72099c35e39dbb8d6
treb <106878698+Trebv@users.noreply.github.com>
2026-01-11 10:43:23 -0500

    Agregando comentarios

--- 2025_1/PC2/problem1.cpp
+++ 2025_1/PC2/problem1.cpp
@@ -1,30 +1,39 @@
 #include <iostream>
 
 int main(){    
-    float numUser = 0;
+    
     int counter = 0;
     float sumaDeNotas = 0;
+	
     do {
-	std::cout << "Digite una nota (0-100):\n";
-	std::cin >> numUser;
-	if (numUser < 0 ){
-	    std::cout << "Saliendo del programa...\n";
-	    break;
-	}
-	if ( numUser < 0 || numUser > 100 ) {
-	    std::cout << "La nota ingresada no esta entre 0 a 100.\n"
-			 "Por favor vuelva a ingresar una nueva nota.\n";
-	    continue;
-	}
-	sumaDeNotas += numUser;
-	counter++;
-    } while (numUser >= 0);
-    if (counter == 0) {
-	std::cout << "No hay datos. Es posible que halla ingresado notas no validas.\n";
+		std::cout << "Digite una nota (0-100):\n";
+		float numUser = 0;
+		std::cin >> numUser;
+
+		// Si el input ingresado es un numero < 0, el programa finaliza.
+		if (numUser < 0 ){ 
+	   		std::cout << "Saliendo del programa...\n";
+	    	return 0;
+		}
+		if ( numUser > 100 ) {
+	    	std::cout << "Error: La nota ingresada no esta entre 0 a 100.\n"
+			 			"Por favor vuelva a ingresar una nueva nota.\n";
+	    	continue;
+		}
+
+		// con cada iteracion se pide un numero. Si el numero es valido [0-100], entonces acumulamos cada numero en la variable sumaDeNotas.
+		sumaDeNotas += numUser;
+
+		// El contador aumenta en 1. Esta variable nos permite saber cuantos numeros validos estamos recibiendo.
+		counter++;
+    } while (true);
+    
+	if (counter == 0) {
+		std::cout << "No hay datos. Es posible que halla ingresado notas no validas.\n";
     } else {
-	std::cout << "cantidad de notas ingresados: " << counter << "\n";
-	float average = sumaDeNotas/(float)counter;
-	std::cout << "Promedio de notas: " << average << "\n";
+		std::cout << "cantidad de notas ingresados: " << counter << "\n";
+		float average = sumaDeNotas/(float)counter;
+		std::cout << "Promedio de notas: " << average << "\n";
     }
     return 0;
 }
