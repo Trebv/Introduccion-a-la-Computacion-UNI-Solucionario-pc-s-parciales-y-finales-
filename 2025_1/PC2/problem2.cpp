fdf6ba71456ea9643b2566ad2c80d964c0cbb617
treb <106878698+Trebv@users.noreply.github.com>
2026-01-11 10:58:12 -0500

    Refactor input handling in problem2.cpp

--- 2025_1/PC2/problem2.cpp
+++ 2025_1/PC2/problem2.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 int main(){
-    int num = 0;
+    
     int counter = 0;
     int sum = 0;
     do {
-	std::cout << "Ingrese un numero: ";
-	std::cin >> num;
-	if (num <= 0 ) { 
-	    std::cout << "Error: Numero no valido\n"
-			 "Por favor digite un numero mayor que 0.\n";
-	    continue;
-	}
+		
+		std::cout << "Ingrese un numero: ";
+		int num = 0;
+		std::cin >> num;
+	
+		if (num <= 0 ) { 
+	    	std::cout << "Error: Numero no valido\n"
+				 "Por favor digite un numero mayor que 0.\n";
+	    	continue;
+		}
     	sum += num;
-	counter++;
+		counter++;
     } while (sum < 50);
 
     std::cout << "Suma total: " << sum << 
-		 "\nCantidad de numeros ingresados: " << counter << "\n";
+		 	"\nCantidad de numeros ingresados: " << counter << "\n";
     return 0;
 }
